5eca712d4eb3cbe02a5f4f96e0b66217f53923d9
olegbilyk <oleg.bilik1994@gmail.com>
2017-04-26 20:15:28 +0300

    fix code style

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     plugins: [
       require('postcss-short'),
       require('postcss-cssnext')({ browsers: [ 'last 3 versions', '> 5%' ] }),
-      require("stylelint"),
+      require('stylelint'),
       require('postcss-browser-reporter')()
     ]
 };
